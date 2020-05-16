e3a55ab09a6d3c92dccf04d314d7c2feea5c6733
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2020-05-16 13:44:38 +0300

    Added bootstrap with sass

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello fucking TypeScript!
-        </p>
-      </header>
+    <div className="container">
+      Hello fucking TypeScript!
+      <br />
+      <button className="btn btn-primary">Fuck!</button>
     </div>
   );
 }
