f7786f8abf119a88d0f8ff0eaabefda63022b7dd
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2020-05-17 10:56:54 +0300

    Simpel SQLite connection has been created.

---
+++ api/Controllers/LessonsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Advancity.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Advancity.Controllers
+{
+  [ApiController]
+  public class LessonsController : ControllerBase
+  {
+
+    private IConfiguration configuration { get; set; }
+
+    public LessonsController(IConfiguration configuration)
+    {
+      this.configuration = configuration;
+    }
+
+    [HttpGet]
+    [Route("api/users")]
+    public List<Lesson> Get()
+    {
+      List<Lesson> lessons = new List<Lesson>();
+      Console.WriteLine(this.configuration.GetSection("ConnectionString").Value);
+      using (IDbConnection db = new SqliteConnection(this.configuration.GetSection("ConnectionString").Value))
+      {
+        lessons = db.Query<Lesson>("SELECT * FROM Lessons").ToList();
+      }
+      return lessons;
+    }
+  }
+}
---
+++ api/Models/Lesson.cs
@@ -0,0 +1,9 @@
+namespace Advancity.Models
+{
+  public class Lesson
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Level { get; set; }
+  }
+}
